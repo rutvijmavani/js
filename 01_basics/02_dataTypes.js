6860dc27534e0920540a0909ab8c241266c88a3f
Rutvij Mavani <rutvijmavani@gmail.com>
2025-06-09 21:57:51 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all JS code as newer version
+
+// alert("hello") // we are using nodejs , not browser
+
+console.log(3 + 3)
+
+console.log("Rutvij");
+
+let name = "rutvij"
+let age = 18
+let isLoggedIn = false
+let state 
+
+// number => 2 ^ 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned \; just like state
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
+
