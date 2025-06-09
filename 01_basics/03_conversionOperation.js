57b4c1aa867902eb9740cf2a8288e84d5857f002
Rutvij Mavani <rutvijmavani@gmail.com>
2025-06-09 22:22:06 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = 33
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN(not an number)
+// true => 1
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = "rutvij"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+// 1 ==> true; 0 => false
+// "" ==> false
+// "rutvij" ==> true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
+
